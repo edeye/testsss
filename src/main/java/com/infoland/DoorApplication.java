09e7d5e651b0a127d107ec3d851f75ff2aa2ca85
liyinghua0321 <413951871@qq.com>
2018-10-26 09:05:31 +0800

    10/26

--- src/main/java/com/infoland/DoorApplication.java
+++ src/main/java/com/infoland/DoorApplication.java
@@ -1,11 +1,13 @@
 package com.infoland;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.infoland.dao")
 public class DoorApplication {
 
 
