716e7c02b196b5c7f723fe7a5a133678ee8ae82e
liyinghua0321 <413951871@qq.com>
2018-11-12 10:13:41 +0800

    11/12

--- src/main/java/com/infoland/Util/ConnectLighThouseService.java
+++ src/main/java/com/infoland/Util/ConnectLighThouseService.java
@@ -5,10 +5,12 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public interface ConnectLighThouseService extends Library {
-	//公共硬盘地址:\\\\10.0.1.188\\Public\\dev\\9-项目资料\\无人值守项目\\Ux64_dllb
-	ConnectLighThouseService CONNECT=Native.load("C:\\Ux64_dllb", ConnectLighThouseService.class);
-	//灯和蜂鸣器开关的方法
-	boolean Usb_Qu_write(byte Qu_index, byte Qu_type, byte[] pData);
-	//检测连接设备
-	int Usb_Qu_Getstate();
+    //公共硬盘地址:\\\\10.0.1.188\\Public\\dev\\9-项目资料\\无人值守项目\\Ux64_dllb
+    ConnectLighThouseService CONNECT = Native.load("C:\\Ux64_dllb", ConnectLighThouseService.class);
+
+    //灯和蜂鸣器开关的方法
+    boolean Usb_Qu_write(byte Qu_index, byte Qu_type, byte[] pData);
+
+    //检测连接设备
+    int Usb_Qu_Getstate();
 }
